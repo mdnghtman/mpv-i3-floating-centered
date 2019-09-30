35d318ed40eb1aa0612bae7337835ab1ceacb4a5
mdnghtman <midnightman@protonmail.com>
2019-09-30 05:18:42 +0200

    Create mpv-i3-floating-centered.lua

---
+++ mpv-i3-floating-centered.lua
@@ -0,0 +1,12 @@
+require 'os'
+require 'io'
+utils = require("mp.utils")
+
+function on_next_center()
+  mpv_windowid = io.popen("xdotool search --pid " .. utils.getpid()):read("*all*")
+  floating = io.popen("xprop -id " .. mpv_windowid):read()
+  if string.match(floating, "FLOATING") then
+    os.execute("i3-msg -q '[id=" .. mpv_windowid  .. "]' move position center")
+  end
+end
+mp.register_event("file-loaded", on_next_center)
