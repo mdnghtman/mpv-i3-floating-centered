0c22558178a00dbaa3da53e16f9876030e40ea00
mdnghtman <midnightman@protonmail.com>
2019-09-30 16:22:49 +0200

    Add fullscreen toggle observation
    Center window when going out of fullscreen.
    Added some checks.

--- mpv-i3-floating-centered.lua
+++ mpv-i3-floating-centered.lua
@@ -3,10 +3,17 @@ require 'io'
 utils = require("mp.utils")
 
 function on_next_center()
-  mpv_windowid = io.popen("xdotool search --pid " .. utils.getpid()):read("*all*")
-  floating = io.popen("xprop -id " .. mpv_windowid):read()
-  if string.match(floating, "FLOATING") then
-    os.execute("i3-msg -q '[id=" .. mpv_windowid  .. "]' move position center")
+  pid = utils.getpid()
+  if pid then
+    mpv_windowid = io.popen("xdotool search --pid " .. pid):read("*all*")
+    if mpv_windowid ~= ""  then
+    floating = io.popen("xprop -id " .. mpv_windowid):read()
+      if string.match(floating, "FLOATING") then
+        os.execute("i3-msg -q '[id=" .. mpv_windowid  .. "]' move position center")
+      end
+    end
   end
 end
+
 mp.register_event("playback-restart", on_next_center)
+mp.observe_property("fullscreen", "bool", on_next_center)
