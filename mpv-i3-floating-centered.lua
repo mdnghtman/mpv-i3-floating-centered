f0ce05ad7d7726c3f216eba3f88ae7c05c8e7e7c
DanklyTuned <>
2021-04-30 10:24:00 -0600

    there is always a pid, and it doesn't change on events

--- mpv-i3-floating-centered.lua
+++ mpv-i3-floating-centered.lua
@@ -1,16 +1,15 @@
 local utils = require 'mp.utils'
 
+local pid = utils.getpid()
+
 function on_next_center()
-  pid = utils.getpid()
-  if pid then
-    mpv_windowid = io.popen("xdotool search --pid " .. pid):read("*all*")
-    if mpv_windowid ~= ""  then
+   mpv_windowid = io.popen("xdotool search --pid " .. pid):read("*all*")
+   if mpv_windowid ~= ""  then
       floating = io.popen("xprop -id " .. mpv_windowid):read()
       if string.match(floating, "FLOATING") then
-        os.execute("i3-msg -q '[id=" .. mpv_windowid  .. "]' move position center")
+         os.execute("i3-msg -q '[id=" .. mpv_windowid  .. "]' move position center")
       end
-    end
-  end
+   end
 end
 
 mp.register_event("playback-restart", on_next_center)
